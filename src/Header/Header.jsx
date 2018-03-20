745bf57ad5413036c7ec51e4de4c8e59be44df2d
Bartosz Platek <Bartosz.Platek@IGT.com>
2018-03-20 16:26:41 +0100

    initial project version

---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="header_container">
+        <div>ccc</div>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/app.jsx'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
